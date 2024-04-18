eac3d2b215b0c5fb51063ebee3577cf37236c4dd
ice_wu <xuhong267@gmail.com>
2024-04-18 23:40:29 +0800

    change data

--- src/views/Table/Table.vue
+++ src/views/Table/Table.vue
@@ -3,6 +3,9 @@
     <TableBox  :titleList="titleList">
       <TableItem :tableData="tableData" @seeDetail="seeDetail"></TableItem>
     </TableBox>
+    <div @click="changeData" class="button">
+
+    </div>
   </div>
 </template>
 
@@ -22,9 +25,8 @@ export default {
           id: 1,
           level: 1,
           name: '张三',
-          age: 18,
+          age: '',
           sex: '男',
-          isExpand: false,
           address: '北京市朝阳区',
           date: '2022-01-01'
         },
@@ -33,7 +35,6 @@ export default {
           level: 1,
           name: '李四',
           age: 19,
-          isExpand: false,
           sex: '女',
           address: '北京市朝阳区',
           date: '2022-01-02',
@@ -138,6 +139,217 @@ export default {
   methods: {
     seeDetail (e) {
       console.log('父组件接收table传的数据', e)
+    },
+    changeData () {
+      console.log('click了')
+      this.tableData = [
+        {
+          id: 1,
+          level: 1,
+          name: 'a张三',
+          age: 18,
+          sex: '男',
+          isExpand: false,
+          address: '北京市朝阳区',
+          date: '2022-01-01'
+        },
+        {
+          id: 2,
+          level: 1,
+          name: 'a李四',
+          age: 19,
+          isExpand: false,
+          sex: '女',
+          address: '北京市朝阳区',
+          date: '2022-01-02',
+          children: [
+            {
+              id: 21,
+              level: 2,
+              name: 'a李四1',
+              age: 19,
+              isExpand: false,
+              sex: '女',
+              address: '北京市朝阳区',
+              date: '2022-01-02',
+              children: [
+                {
+                  id: 211,
+                  level: 3,
+                  name: 'a李四11',
+                  age: 19,
+                  isExpand: false,
+                  sex: '女',
+                  address: '北京市朝阳区',
+                  date: '2022-01-02',
+                  children: []
+                }
+              ]
+            },
+            {
+              id: 22,
+              level: 2,
+              name: 'a李四21',
+              age: 19,
+              isExpand: false,
+              sex: '女',
+              address: '北京市朝阳区',
+              date: '2022-01-02',
+              children: [
+                {
+                  id: 311,
+                  level: 3,
+                  name: 'a李四31',
+                  age: 19,
+                  isExpand: false,
+                  sex: '女',
+                  address: '北京市朝阳区',
+                  date: '2022-01-02',
+                  children: []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          level: 1,
+          id: 3,
+          name: 'a王五',
+          age: 20,
+          isExpand: false,
+          sex: '男',
+          address: '北京市朝阳区',
+          date: '2022-01-03',
+          children: [
+            {
+              level: 2,
+              id: 31,
+              name: 'a王五1',
+              age: 20,
+              isExpand: false,
+              sex: '男',
+              address: '北京市朝阳区',
+              date: '2022-01-03',
+              children: [
+                {
+                  level: 3,
+                  id: 311,
+                  name: 'a王五11',
+                  age: 20,
+                  isExpand: false,
+                  sex: '男',
+                  address: '北京市朝阳区',
+                  date: '2022-01-03',
+                  children: []
+                },
+                {
+                  level: 3,
+                  id: 312,
+                  name: 'a王五12',
+                  age: 20,
+                  isExpand: false,
+                  sex: '男',
+                  address: '北京市朝阳区',
+                  date: '2022-01-03',
+                  children: []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          level: 1,
+          id: 4,
+          name: 'a王五',
+          age: 20,
+          isExpand: false,
+          sex: '男',
+          address: '北京市朝阳区',
+          date: '2022-01-03',
+          children: [
+            {
+              level: 2,
+              id: 41,
+              name: 'a王五1',
+              age: 20,
+              isExpand: false,
+              sex: '男',
+              address: '北京市朝阳区',
+              date: '2022-01-03',
+              children: [
+                {
+                  level: 3,
+                  id: 411,
+                  name: 'a王五11',
+                  age: 20,
+                  isExpand: false,
+                  sex: '男',
+                  address: '北京市朝阳区',
+                  date: '2022-01-03',
+                  children: []
+                },
+                {
+                  level: 3,
+                  id: 412,
+                  name: 'a王五12',
+                  age: 20,
+                  isExpand: false,
+                  sex: '男',
+                  address: '北京市朝阳区',
+                  date: '2022-01-03',
+                  children: []
+                }
+              ]
+            }
+          ]
+        },
+        {
+          level: 1,
+          id: 5,
+          name: 'a王五',
+          age: 20,
+          isExpand: false,
+          sex: '男',
+          address: '北京市朝阳区',
+          date: '2022-01-03',
+          children: [
+            {
+              level: 2,
+              id: 51,
+              name: 'a王五1',
+              age: 20,
+              isExpand: false,
+              sex: '男',
+              address: '北京市朝阳区',
+              date: '2022-01-03',
+              children: [
+                {
+                  level: 3,
+                  id: 511,
+                  name: 'a王五11',
+                  age: 20,
+                  isExpand: false,
+                  sex: '男',
+                  address: '北京市朝阳区',
+                  date: '2022-01-03',
+                  children: []
+                },
+                {
+                  level: 3,
+                  id: 512,
+                  name: 'a王五12',
+                  age: 20,
+                  isExpand: false,
+                  sex: '男',
+                  address: '北京市朝阳区',
+                  date: '2022-01-03',
+                  children: []
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
   }
 
@@ -145,5 +357,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.button{
+  width: 100px;
+  height: 50px;
+  border-radius:25px ;
+  background-color: #e76d6d;
+  cursor: pointer;
+}
 </style>>
--- src/views/Table/TableItem.vue
+++ src/views/Table/TableItem.vue
@@ -5,15 +5,15 @@
         <div class="id width40 rcc" :style=" {'padding-left':getPaddingLeft(item.level)}">
           <div class="title position-relative">
             <div class="title-detail display-inline-block position-relative">{{item.id}}
-              <span v-if="item.children && item.children.length" class="expand" @click="item.isExpand=!item.isExpand"></span>
+              <span v-if="item.children && item.children.length" class="expand" @click="toggle(item)"></span>
             </div>
           </div>
         </div>
         <div class="name width12 cursor-pointer rcc" @click="seeDetail(item)">{{item.name}}</div>
-        <div class="age width12 rcc">{{item.age}}</div>
-        <div class="sex width12 rcc">{{item.sex}}</div>
-        <div class="address width12 rcc">{{item.address}}</div>
-        <div class="date width12 rcc">{{item.date}}</div>
+        <div class="age width12 rcc">{{item.age||'--'}}</div>
+        <div class="sex width12 rcc">{{item.sex ||'--'}}</div>
+        <div class="address width12 rcc">{{item.address ||'--'}}</div>
+        <div class="date width12 rcc">{{item.date ||'--'}}</div>
       </div>
       <div v-if="item.isExpand===true" class="level-2">
         <TableItem :tableData="item.children" @seeDetail="seeDetail"></TableItem>
@@ -27,12 +27,13 @@ import { addIsExpand } from '@/utils/common.js'
 import TableItem from '@/views/Table/TableItem.vue'
 export default {
   name: 'TableItem',
-  comments: {
+  components: {
     TableItem
   },
   props: {
     tableData: {
-      type: Array
+      type: Array,
+      default: () => []
     }
   },
   data () {
@@ -40,58 +41,66 @@ export default {
       hasIsExpandTableData: []
     }
   },
-  // watch: {
-  //   tableData: {
-  //     handler (newVal) {
-  //       this.setNewData(newVal)
-  //     },
-  //     deep: true,
-  //     immediate: true
-  //   }
-  // },
-  mounted () {
-    const res = addIsExpand(JSON.parse(JSON.stringify(this.tableData)))
-    // debugger
-    console.log(res)
-    this.hasIsExpandTableData = res
-    // this.setNewData(this.copiedData)
-    // console.log(this.hasIsExpandTableData)
-  },
-  computed: {
-    copiedData () {
-      return JSON.parse(JSON.stringify(this.tableData))
+  watch: {
+    tableData: {
+      handler (newVal) {
+        this.setNewData(newVal)
+      },
+      deep: true,
+      immediate: true
     }
   },
+  /**
+   * todo:改变父组件的数据，表格重新渲染
+  */
+  // mounted () {
+  //   const res = addIsExpand(JSON.parse(JSON.stringify(this.tableData)))
+  //   // debugger
+  //   console.log(res)
+  //   this.hasIsExpandTableData = res
+  //   // this.setNewData(this.copiedData)
+  //   // console.log(this.hasIsExpandTableData)
+  // },
+  // computed: {
+  //   copiedData () {
+  //     return addIsExpand(JSON.parse(JSON.stringify(this.tableData)))
+  //   }
+  // },
   methods: {
+    toggle (item) {
+      item.isExpand = !item.isExpand
+      console.log(item.isExpand)
+    },
     seeDetail (item) {
+      // console.log('see')
       this.$emit('seeDetail', item)
     },
     getPaddingLeft (level) {
       return (level - 1) * 55 + 'px'
     },
-    addIsEdit (arr) {
-      return arr.map(item => {
-        // 创建一个新对象，保持原对象的属性不变
-        const newItem = { ...item, isExpand: false }
-        // 如果当前数组元素有 children 属性且是数组，则递归处理 children 数组元素
-        if (item.children && Array.isArray(item.children) && item.children.length) {
-          newItem.children = this.addIsEdit(item.children)
-        }
-        return newItem
-      })
-    },
-    addIsExpandToTableData (tableData) {
-      return tableData.map(item => {
-        const newItem = {
-          ...item,
-          isExpand: false
-        }
-        if (item.children && item.children.length) {
-          newItem.children = this.addIsExpandToTableData(item.children)
-        }
-        return newItem
-      })
-    },
+    // addIsEdit (arr) {
+    //   return arr.map(item => {
+    //     // 创建一个新对象，保持原对象的属性不变
+    //     const newItem = { ...item, isExpand: false }
+    //     // 如果当前数组元素有 children 属性且是数组，则递归处理 children 数组元素
+    //     if (item.children && Array.isArray(item.children) && item.children.length) {
+    //       newItem.children = this.addIsEdit(item.children)
+    //     }
+    //     return newItem
+    //   })
+    // },
+    // addIsExpandToTableData (tableData) {
+    //   return tableData.map(item => {
+    //     const newItem = {
+    //       ...item,
+    //       isExpand: false
+    //     }
+    //     if (item.children && item.children.length) {
+    //       newItem.children = this.addIsExpandToTableData(item.children)
+    //     }
+    //     return newItem
+    //   })
+    // },
     setNewData (tableData) {
       this.hasIsExpandTableData = addIsExpand(tableData)
     }
